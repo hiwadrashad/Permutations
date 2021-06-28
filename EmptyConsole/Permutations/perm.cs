ef470e690b473f9fb538da0b3f05b858fbdf4495
hiwad rashad <hiwadrashad112@hotmail.com>
2021-06-28 16:27:22 +0200

    Add project files.

---
+++ EmptyConsole/Permutations/perm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EmptyConsole.Permutations
+{
+
+    public class TestSerialization
+    {
+        public static void main(string[] args)
+        {
+            Console.WriteLine("something");
+        }
+    
+    }
+    public static class perm
+    {
+
+        public static IEnumerable<IEnumerable<T>> Permute<T>(this IEnumerable<T> sequence)
+        {
+            if (sequence == null)
+            {
+                yield break;
+            }
+
+            var list = sequence.ToList();
+
+            if (!list.Any())
+            {
+                yield return Enumerable.Empty<T>();
+            }
+            else
+            {
+                var startingElementIndex = 0;
+
+                foreach (var startingElement in list)
+                {
+                    var index = startingElementIndex;
+                    var remainingItems = list.Where((e, i) => i != index);
+
+                    foreach (var permutationOfRemainder in remainingItems.Permute())
+                    {
+                        yield return permutationOfRemainder.Prepend(startingElement);
+                    }
+
+                    startingElementIndex++;
+                }
+            }
+        }
+    }
+}
---
+++ EmptyConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmptyConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] source = new string[] { "dog", "cat", "mouse" };
+            var item = Permutations.perm.Permute(source);
+            foreach (var item2d in item)
+            {
+                string stringtowrite = "";
+                foreach (var item3d in item2d)
+                {
+                    stringtowrite += item3d + " ";
+                }
+                Console.WriteLine(stringtowrite);
+                stringtowrite = "";
+            }
+
+        }
+    }
+}
